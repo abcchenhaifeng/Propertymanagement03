175080f1dca6125ccde42559ca8cbda324cda8b4
ychenli <1056560004@qq.com>
2019-08-07 16:32:00 +0800

    2019/08/07 第一次提交(全部)

---
+++ PropertyManagement03/src/main/java/cn/yichen/PropertyManagement03/PropertyManagement03Application.java
@@ -0,0 +1,13 @@
+package cn.yichen.PropertyManagement03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagement03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagement03Application.class, args);
+	}
+
+}
