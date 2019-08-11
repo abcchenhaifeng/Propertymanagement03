c9fbd8c9758e1629b5cb98706d8852570cf9518a
ychenli <1056560004@qq.com>
2019-08-11 13:32:55 +0800

    初步完成src/main/resources/static/index.html

--- PropertyManagement03/src/main/resources/static/js/index.js
+++ PropertyManagement03/src/main/resources/static/js/index.js
@@ -3,3 +3,65 @@
  * @author YiChen(李冠永)
  * @Description 首页前端控制JS
  */
+
+let default_alert_info;
+$(function() {
+
+	default_alert_info = $("#alert_info").text();
+	
+	$(".footer #date_year").text(new Date().getFullYear());
+	
+	setMessage("今天是： " + new Date().toLocaleDateString(), 5000);
+
+	$(".tablesorter").tablesorter();
+	$('.column').equalHeight();
+
+	// When page loads...
+	$(".tab_content").hide(); // Hide all content
+	$("ul.tabs li:first").addClass("active").show(); // Activate first tab
+	$(".tab_content:first").show(); // Show first tab content
+
+	// On Click Event
+	$("ul.tabs li").click(function() {
+
+		$("ul.tabs li").removeClass("active"); // Remove any "active" class
+		$(this).addClass("active"); // Add "active" class to selected tab
+		$(".tab_content").hide(); // Hide all tab content
+
+		var activeTab = $(this).find("a").attr("href"); // Find the href
+		// attribute value to
+		// identify the active
+		// tab + content
+		$(activeTab).fadeIn(); // Fade in the active ID content
+		return false;
+	});
+
+});
+
+function setMessage(text, timeout, flag) {
+	showMessage($("#alert_info"), text, timeout, flag);
+}
+
+function showMessage(e, text, timeout, flag) {
+
+	if (flag || sessionStorage.getItem(e) === ''
+			|| sessionStorage.getItem(e) === null) {
+
+		if (flag)
+			sessionStorage.setItem(e, text);
+
+		$(e).stop().fadeTo(200, 0).html(text).fadeTo(200, 1);
+		if (timeout === null)
+			timeout = 5000;
+		defaultMessage(e, timeout);
+	}
+}
+
+function defaultMessage(e, timeout) {
+	if (timeout === null)
+		timeout = 5000;
+	window.setTimeout(function() {
+		sessionStorage.removeItem(e)
+		$(e).stop().fadeTo(200, 0).html(default_alert_info).fadeTo(200, 1);
+	}, timeout);
+}
---
+++ PropertyManagement03/src/main/resources/static/js/user/main.js
@@ -0,0 +1,5 @@
+/**
+ * @date: 2019年8月11日 上午10:44:09
+ * @author: YiChen(李冠永)
+ * @Description: 系统操作员前端控制JS
+ */
