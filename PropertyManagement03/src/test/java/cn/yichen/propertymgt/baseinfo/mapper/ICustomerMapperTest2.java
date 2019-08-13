86657b0dde25655e112a137d21b5bb996ae0d7e4
lihaihong <1305102684@qq.com>
2019-08-13 11:37:19 +0800

    分页

---
+++ PropertyManagement03/src/test/java/cn/yichen/propertymgt/baseinfo/mapper/ICustomerMapperTest2.java
@@ -0,0 +1,19 @@
+package cn.yichen.propertymgt.baseinfo.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ICustomerMapperTest2 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSelectCustomerByIdandHomeWithPage() {
+		fail("Not yet implemented");
+	}
+
+}
