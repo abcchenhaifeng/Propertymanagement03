68c3b864c40c73b748c28737af9adfa585b5aa6f
ychenli <1056560004@qq.com>
2019-09-04 18:35:33 +0800

    修正 (paytype[vue.js]) 在其他平台无法运行[未测试](2019年9月4日18:35:30)

--- PropertyManagement03/src/main/resources/static/paytype/vue.config.js
+++ PropertyManagement03/src/main/resources/static/paytype/vue.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
 
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+	publicPath: process.env.NODE_ENV === 'production' ? './paytype/dist' : './',
+	assetsDir: 'assets',
 	outputDir: 'dist',
+	indexPath: 'main.html',
+	runtimeCompiler: true,
+	 productionSourceMap: false,
 }
